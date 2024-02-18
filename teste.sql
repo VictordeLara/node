57ff2cf394a90425789fc8958abb6831e38c717e
VictordeLara <victorlara2010@gmail.com>
2024-02-18 19:55:36 -0300

    node

---
+++ teste.sql
@@ -0,0 +1,14 @@
+CREATE TABLE usuarios(
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    idade INT
+
+
+
+);
+
+INSERT INTO usuarios(nome, email, idade) VALUES(
+    "Aang",
+    "email@teste1.com",
+    20
+);
